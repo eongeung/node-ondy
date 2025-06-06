7b531beb8dd45faa58cd62b65a3d516eda60ca5a
Yeongheun <iyoungheuni@gmail.com>
2025-06-06 22:51:43 +0900

    feat: Add splash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.node_ondy"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
